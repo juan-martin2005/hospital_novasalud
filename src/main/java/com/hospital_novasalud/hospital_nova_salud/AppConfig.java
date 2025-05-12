59d44229e4162ae75d29454b23d88d86c0a48d18
Diego Malasquez <N00319083@upn.pe>
2025-05-12 13:02:33 -0500

    Configuracion del CORS

---
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/AppConfig.java
@@ -0,0 +1,24 @@
+package com.hospital_novasalud.hospital_nova_salud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig{
+
+    @Bean
+    public WebMvcConfigurer corsConfiguration(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                .allowedOrigins("http:/localhost:4200")
+                .allowedHeaders("GET","POST","PUT","DELETE")
+                .allowCredentials(true);
+            }
+        };
+    }
+
+}
