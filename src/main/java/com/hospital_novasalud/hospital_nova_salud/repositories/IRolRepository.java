703753012c67f3940179f313b7a70a9e598febd4
Juan <martin.gonzaless2005@gmail.com>
2025-05-18 02:09:06 -0500

    pequeños cambios

--- src/main/java/com/hospital_novasalud/hospital_nova_salud/repositories/IRolRepository.java
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/repositories/IRolRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.hospital_novasalud.hospital_nova_salud.models.Rol;
 
 public interface IRolRepository extends CrudRepository<Rol, Integer> {
+    Rol findByNombreRol(String nameRol);
     Rol findById(int id);
     boolean existsById(int id);
 }
--- src/main/java/com/hospital_novasalud/hospital_nova_salud/repositories/IUsuarioRepository.java
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/repositories/IUsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.hospital_novasalud.hospital_nova_salud.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.hospital_novasalud.hospital_nova_salud.models.Usuario;
@@ -8,6 +9,7 @@ import com.hospital_novasalud.hospital_nova_salud.models.Usuario;
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long>{
     boolean existsByNombreUsuaAndContrasena(String n, String c);
     boolean existsByNombreUsua(String n);
+    Optional<Usuario> findByNombreUsua(String usuario);
     boolean existsByDni(String dni);
     Usuario findByDni(String dni);
     List<Usuario> findAllByEstadoId(int estado);
