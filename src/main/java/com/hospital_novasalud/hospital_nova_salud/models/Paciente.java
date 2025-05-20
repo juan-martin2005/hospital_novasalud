db672950584df5197d25871fb0eac94e277eee33
Juan <martin.gonzaless2005@gmail.com>
2025-05-19 20:50:09 -0500

    Se agrego el atributo dni a la clase de paciente

--- src/main/java/com/hospital_novasalud/hospital_nova_salud/models/Paciente.java
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/models/Paciente.java
@@ -7,7 +7,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="pacientes")
@@ -15,6 +17,9 @@ public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
+    @Size(min=7, max=8)
+    private String dni;
     @NotNull
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
@@ -22,8 +27,9 @@ public class Paciente {
     
     public Paciente() {
     }
-    public Paciente(Long id, Usuario usuario) {
+    public Paciente(Long id, String dni, Usuario usuario) {
         this.id = id;
+        this.dni = dni;
         this.usuario = usuario;
     }
     public Long getId() {
@@ -31,6 +37,12 @@ public class Paciente {
     }
     public void setId(Long id) {
         this.id = id;
+    }
+    public String getDni() {
+        return dni;
+    }
+    public void setDni(String dni) {
+        this.dni = dni;
     }
     public Usuario getUsuario() {
         return usuario;
