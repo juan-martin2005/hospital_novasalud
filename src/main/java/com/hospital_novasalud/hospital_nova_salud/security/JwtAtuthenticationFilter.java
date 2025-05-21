e7a258bc97c5fa6b6f099eaa5bfd6a6fa302a6d0
Juan <martin.gonzaless2005@gmail.com>
2025-05-21 02:12:39 -0500

    Configuracion y validacion del JWT

---
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/security/JwtAtuthenticationFilter.java
@@ -0,0 +1,107 @@
+package com.hospital_novasalud.hospital_nova_salud.security;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hospital_novasalud.hospital_nova_salud.models.Usuario;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import static com.hospital_novasalud.hospital_nova_salud.security.TokenJwtConfig.*;
+
+public class JwtAtuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    private AuthenticationManager authenticationManager;
+
+
+    public JwtAtuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        
+        Usuario usuario = null;
+        String contrasena = null;
+        String nombreUsuaio = null;
+
+        try {
+            usuario = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
+            nombreUsuaio = usuario.getNombreUsua();
+            contrasena = usuario.getContrasena();
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(nombreUsuaio, contrasena);
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+
+        User usuario = (User) authResult.getPrincipal();
+        String nombreUsuario = usuario.getUsername();
+        String rol = authResult.getAuthorities().iterator().next().getAuthority();
+
+        Claims claims = Jwts.claims().add("role", rol).build();
+        String token = Jwts.builder()
+                .subject(nombreUsuario)
+                .claims(claims)
+                .issuedAt(new Date())
+                .signWith(SECRET_KEY)
+                .compact();
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("usuario", nombreUsuario);
+        body.put("token", token);
+        body.put("rol", rol);
+        body.put("mensaje", "Se ha iniciado sesion correctamente");
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(200);
+
+
+    }
+
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        
+        Map<String, String> body = new HashMap<>();
+        body.put("error", failed.getMessage());
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(401);
+    }
+
+    
+    
+
+}
---
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/security/JwtValidationFilter.java
@@ -0,0 +1,67 @@
+package com.hospital_novasalud.hospital_nova_salud.security;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import static com.hospital_novasalud.hospital_nova_salud.security.TokenJwtConfig.*;
+
+public class JwtValidationFilter extends BasicAuthenticationFilter{
+
+    public JwtValidationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String header = request.getHeader(HEADER_AUTHORIZATION);
+
+        if(header == null || !header.startsWith(PREFIX_TOKEN)){
+            chain.doFilter(request, response);
+
+            return;
+        }
+
+        String token = header.replace(PREFIX_TOKEN, "");
+        
+        try {
+            Claims claims = Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+            String username = claims.getSubject();
+            Object rolClaims = claims.get("role");
+
+            String rol = rolClaims.toString();
+            List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(rol));
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            chain.doFilter(request, response);
+
+        } catch (JwtException e) {
+            Map<String, String> body = new HashMap<>();
+            body.put("error", e.getMessage());
+            body.put("mensaje", "El token JWT es inválido");
+
+            response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+            response.setStatus(401);
+            response.setContentType(CONTENT_TYPE);
+
+        }
+    }
+}
