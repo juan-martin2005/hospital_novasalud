18f5824b3db502c90836e3a0b1e7463b057c3426
Juan <martin.gonzaless2005@gmail.com>
2025-05-21 02:13:13 -0500

    Endpoints restringidos

--- src/main/java/com/hospital_novasalud/hospital_nova_salud/security/SecurityConfig.java
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.hospital_novasalud.hospital_nova_salud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,18 +15,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    // Configuracion de quienes pueden acceder a los endpoints
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http.authorizeHttpRequests(auth -> 
-            auth.requestMatchers(HttpMethod.GET,"/api/usuarios/listar").permitAll()
-            .requestMatchers(HttpMethod.POST,"api/usuarios/registrar").permitAll()
-            .requestMatchers(HttpMethod.DELETE,"api/usuarios/eliminar").permitAll()
+        return http.authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/usuarios/**").hasAuthority("ROL_ADMIN")
+            .requestMatchers("/api/doctor/**").hasAuthority("ROL_ADMIN")
+            .requestMatchers("/api/recepcionista/**").hasAuthority("ROL_ADMIN")
+            .requestMatchers(HttpMethod.DELETE, "/api/paciente/eliminar/{id}").hasAuthority("ROL_ADMIN")
+            .requestMatchers(HttpMethod.GET, "/api/paciente/listar").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/paciente/registrar").permitAll()
+            //Aun no realizadas
+            .requestMatchers(HttpMethod.GET, "/api/cita-medica/listar").hasAuthority("ROL_PACIENTE")
+            .requestMatchers(HttpMethod.POST, "/api/cita-medica/registrar").hasAuthority("ROL_PACIENTE")
+            .requestMatchers(HttpMethod.PUT, "/api/cita-medica/modificar/{id}").hasAuthority("ROL_PACIENTE")
+            .requestMatchers(HttpMethod.DELETE, "/api/cita-medica/eliminar/{id}").hasAuthority("ROL_PACIENTE")
             .anyRequest().authenticated())
+            .addFilter(new JwtAtuthenticationFilter(authenticationManager()))
+            .addFilter(new JwtValidationFilter(authenticationManager()))
             .csrf(config -> config.disable())
             .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .build();
