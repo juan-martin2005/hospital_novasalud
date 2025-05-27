84a1da6b18f2455778aa57b6ed8fa5f376edbfe5
Juan <martin.gonzaless2005@gmail.com>
2025-05-27 11:55:48 -0500

    Se agrego la configuracion de Cors

--- src/main/java/com/hospital_novasalud/hospital_nova_salud/security/SecurityConfig.java
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hospital_novasalud.hospital_nova_salud.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -36,7 +41,7 @@ public class SecurityConfig {
             .requestMatchers("/api/recepcionista/**").hasAuthority("ROL_ADMIN")
             .requestMatchers(HttpMethod.DELETE, "/api/paciente/eliminar/{id}").hasAuthority("ROL_ADMIN")
             .requestMatchers(HttpMethod.GET, "/api/paciente/listar").permitAll()
-            .requestMatchers(HttpMethod.POST, "/api/paciente/registrar").anonymous()
+            .requestMatchers(HttpMethod.POST, "/api/paciente/registrar").permitAll()
             //Aun no realizadas
             .requestMatchers(HttpMethod.GET, "/api/cita-medica/listar").hasAuthority("ROL_PACIENTE")
             .requestMatchers(HttpMethod.POST, "/api/cita-medica/registrar").hasAuthority("ROL_PACIENTE")
@@ -46,7 +51,21 @@ public class SecurityConfig {
             .addFilter(new JwtAtuthenticationFilter(authenticationManager()))
             .addFilter(new JwtValidationFilter(authenticationManager()))
             .csrf(config -> config.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http//localhost:4200"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+
     }
 }
