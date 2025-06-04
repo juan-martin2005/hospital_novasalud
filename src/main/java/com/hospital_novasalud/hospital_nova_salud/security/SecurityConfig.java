75d58a2a2179a05e1e7e3138caaf0a44c017a842
Juan <martin.gonzaless2005@gmail.com>
2025-06-04 18:58:45 -0500

    xd

--- src/main/java/com/hospital_novasalud/hospital_nova_salud/security/SecurityConfig.java
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http//localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         configuration.setAllowCredentials(true);
