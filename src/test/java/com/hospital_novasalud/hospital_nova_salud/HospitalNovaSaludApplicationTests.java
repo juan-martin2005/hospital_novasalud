3810e9c87f5c64d61ffed24705dfa6bda4b8c98c
Juan <martin.gonzaless2005@gmail.com>
2025-06-13 03:06:13 -0500

    new actualizacion

--- src/test/java/com/hospital_novasalud/hospital_nova_salud/HospitalNovaSaludApplicationTests.java
+++ src/test/java/com/hospital_novasalud/hospital_nova_salud/HospitalNovaSaludApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HospitalNovaSaludApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
