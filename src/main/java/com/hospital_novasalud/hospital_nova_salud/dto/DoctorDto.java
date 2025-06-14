dac644101c2e73e2e37f89cc3bfbc997ef74a102
Juan <martin.gonzaless2005@gmail.com>
2025-06-14 01:58:59 -0500

    doctorDto

--- src/main/java/com/hospital_novasalud/hospital_nova_salud/dto/DoctorDto.java
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/dto/DoctorDto.java
@@ -3,19 +3,21 @@ package com.hospital_novasalud.hospital_nova_salud.dto;
 import com.hospital_novasalud.hospital_nova_salud.models.Doctor;
 
 public class DoctorDto {
+    private final Long id;
     private final String nombre;
     private final String apellido;
-    private final String numero;
-    private final char sexo;
     private final String especialidad;
 
 
     public DoctorDto(Doctor doc){
+        this.id = doc.getId();
         this.nombre = doc.getUsuario().getNombre();
         this.apellido = doc.getUsuario().getApellido();
-        this.numero = doc.getUsuario().getNumero();
-        this.sexo = doc.getUsuario().getSexo();
         this.especialidad = doc.getEspecialidad().getNombre();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getNombre() {
@@ -24,14 +26,6 @@ public class DoctorDto {
 
     public String getApellido() {
         return apellido;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public char getSexo() {
-        return sexo;
     }
 
     public String getEspecialidad() {
