40485416f7c314f9775e6154e7513d383c6393a8
Juan <martin.gonzaless2005@gmail.com>
2025-07-03 21:53:37 -0500

    ,

--- src/main/java/com/hospital_novasalud/hospital_nova_salud/dto/MedicamentoDto.java
+++ src/main/java/com/hospital_novasalud/hospital_nova_salud/dto/MedicamentoDto.java
@@ -1,11 +1,12 @@
 package com.hospital_novasalud.hospital_nova_salud.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record MedicamentoDto (String id,
                               @NotBlank String nombre,
                               @NotBlank String descripcion,
-                              @NotBlank int cantidad,
+                              @NotNull Integer cantidad,
                               @NotBlank String fecha_ingreso,
                               @NotBlank String fecha_vencimiento,
                               @NotBlank String precio_unitario){
